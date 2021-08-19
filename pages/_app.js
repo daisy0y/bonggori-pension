350e97e6927561a20778d176d33c160b3b41fbcc
daisy0y <61371367+daisy0y@users.noreply.github.com>
2021-08-19 23:15:55 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import "../styles/globals.css";
 
+const them = {
+    testColor: "#ff4c01",
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={them}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
