c7017953d992926926f8161023780629c3222d91
2h5i <dev.2h5i@gmail.com>
2021-08-28 20:04:52 +0900

    feat: add untracked files

--- utils/users.js
+++
@@ -1,8 +0,0 @@
-import { firestore } from "./initFirebase";
-
-const getUsers = async () => {
-    const snapshot = await firestore.collection("user").get();
-    snapshot.docs.forEach(doc => console.log(doc));
-};
-
-export { getUsers };
